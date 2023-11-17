98897e4b4427a1568f65625b309924a5c386c57f
negyeliczkysandor <sandor.negyeliczky@gmail.com>
2023-11-17 12:13:16 +0100

    add: final run

--- src/100Doors/progress.test.ts
+++ src/100Doors/progress.test.ts
@@ -1,66 +1,88 @@
-import {init, toggle, visitor, visitAll} from "./progress";
-import {Door} from "./types";
+import { init, toggle, visitor, visitAll, run, printer } from "./progress";
+import * as progress from "./progress";
+import { Door } from "./types";
 
 describe("tests of 100 doors:", () => {
-    const closedDoor: Door = {
-        index: 1,
-        isOpen: false
-    }
+  const closedDoor: Door = {
+    index: 1,
+    isOpen: false,
+  };
 
-    const openDoor: Door = {
-        index: 1,
-        isOpen: true
-    }
+  const openDoor: Door = {
+    index: 1,
+    isOpen: true,
+  };
 
-    const testArray = [closedDoor, closedDoor]
+  const testArray = [closedDoor, closedDoor];
 
-    it("should has an init function", () => {
-        expect(init).toBeDefined();
+  it("should has an init function", () => {
+    expect(init).toBeDefined();
+  });
+
+  it("should has init and retun an array", () => {
+    expect(init()).toBeInstanceOf(Array);
+  });
+
+  it("should have an array with 100 objects", () => {
+    expect(init().length).toBe(100);
+  });
+
+  it("should have door type objects", () => {
+    const results = init();
+    expect(results[0]).toEqual({
+      index: expect.any(Number),
+      isOpen: expect.any(Boolean),
     });
+  });
 
-    it("should has init and retun an array", () => {
-        expect(init()).toBeInstanceOf(Array);
-    });
+  it("should have a toggle function to open or close doors", () => {
+    expect(toggle).toBeDefined();
+  });
 
-    it("should have an array with 100 objects", () => {
-        expect(init().length).toBe(100);
-    });
+  it("should close an open door and open a closed door", () => {
+    expect(toggle(closedDoor).isOpen).toEqual(true);
+    expect(toggle(openDoor).isOpen).toEqual(false);
+  });
 
-    it("should have door type objects", () => {
-        const results = init();
-        expect(results[0]).toEqual({
-            index: expect.any(Number),
-            isOpen: expect.any(Boolean),
-        });
-    });
+  it("should have a visitor ", () => {
+    expect(visitor).toBeDefined();
+  });
 
-    it('should have a toggle function to open or close doors', () => {
-        expect(toggle).toBeDefined()
-    })
+  it("should toggle doors on second index", () => {
+    expect(visitor(1, testArray)[1]).toEqual(openDoor);
+  });
 
-    it('should close an open door and open a closed door', () => {
+  it("should have a visit100Time", () => {
+    expect(visitAll).toBeDefined();
+  });
 
-        expect(toggle(closedDoor).isOpen).toEqual(true)
-        expect(toggle(openDoor).isOpen).toEqual(false)
-    })
+  it("should close the last door on the first run ", () => {
+    expect(visitAll(1, testArray)[1]).toEqual(openDoor);
+  });
 
-    it('should have a visitor ', () => {
-        expect(visitor).toBeDefined()
-    })
+  it("should visit the given array 2 times", () => {
+    expect(visitAll(2, testArray)[1]).toEqual(closedDoor);
+  });
 
-    it('should toggle doors on second index', () => {
-        expect(visitor(1, testArray)[1]).toEqual(openDoor)
-    })
+  it("should call init in run", () => {
+    const initSpy = jest.spyOn(progress, "init");
+    run();
+    expect(initSpy).toHaveBeenCalled();
+  });
 
-    it('should have a visit100Time', () => {
-        expect(visitAll).toBeDefined()
-    })
+  it("should have to visit the doors with the length of init", () => {
+    const initLength = init().length;
+    const visitorSpy = jest.spyOn(progress, "visitor");
+    run();
+    expect(visitorSpy).toHaveBeenCalledTimes(initLength);
+  });
 
-    it('should close the last door on the first run ', () => {
-        expect(visitAll(1, testArray)[1]).toEqual(openDoor)
-    })
+  it("should print @ for open doors # for close doors", () => {
+    const printDoors = [openDoor, closedDoor];
+    expect(printer(printDoors)).toEqual("@#");
+  });
 
-    it('should visit the given array 2 times', () => {
-        expect(visitAll(2, testArray)[1]).toEqual(closedDoor)
-    })
+  it("should return as the final string first 5 char", () => {
+    expect(run().slice(0, 5)).toEqual("@##@#");
+  });
 });
--- src/100Doors/progress.ts
+++ src/100Doors/progress.ts
@@ -1,29 +1,37 @@
-import {Door} from "./types";
+import { Door } from "./types";
 
 export const init = (): Array<Door> =>
-    new Array(100).fill(undefined).map((item, index) => ({
-        index: index,
-        isOpen: false,
-    }));
+  new Array(100).fill(undefined).map((item, index) => ({
+    index: index,
+    isOpen: false,
+  }));
 
 export const toggle = (door: Door): Door => ({
-    ...door,
-    isOpen: !door.isOpen
-})
+  ...door,
+  isOpen: !door.isOpen,
+});
 
-
-export const visitor = (doorIndex: number, doors: Door[]): Door[] => doors.map(
-    (door, index) => {
-        return (index + 1) % doorIndex === 0 ? toggle(door) : door
-    }
-)
+export const visitor = (doorIndex: number, doors: Door[]): Door[] =>
+  doors.map((door, index) => {
+    return (index + 1) % doorIndex === 0 ? toggle(door) : door;
+  });
 
 export const visitAll = (visits: number, doors: Door[]): Door[] => {
-    let counter = 1
-    let doorLine = doors
-    while (counter <= visits) {
-        doorLine = visitor(counter, doorLine)
-        counter++
-    }
-    return doorLine
-}
+  let counter = 1;
+  let doorLine = doors;
+  while (counter <= visits) {
+    doorLine = visitor(counter, doorLine);
+    counter++;
+  }
+  return doorLine;
+};
+
+export const printer = (doors: Door[]) =>
+  doors.map((door) => (door.isOpen ? "@" : "#")).join("");
+
+export const run = (): string => {
+  const doors = init();
+  const visits = doors.length;
+  const afterVisit = visitAll(visits, doors);
+  return printer(afterVisit);
+};
