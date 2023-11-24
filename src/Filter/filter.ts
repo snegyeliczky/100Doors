2333778c9470024fadd3c7c7c1093f623fabc74b
negyeliczkysandor <sandor.negyeliczky@gmail.com>
2023-11-24 16:08:16 +0100

    add: array filter

--- src/Filter/filter.ts
+++ src/Filter/filter.ts
@@ -1,6 +1,6 @@
 export const filter = <Type>(
   input: Array<Type>,
-  callback: (value: any) => boolean,
+  callback: (value: Type) => boolean,
 ): Type[] => {
   let newArray: Type[] = [];
   input.forEach((value) => callback(value) && newArray.push(value));
