8bcb89e991df92ae0d0ceefca7ba52aaea86764f
negyeliczkysandor <sandor.negyeliczky@gmail.com>
2023-11-27 15:42:42 +0100

    add: generic type to function cache

--- src/jsCache/jsCache.ts
+++ src/jsCache/jsCache.ts
@@ -1,14 +1,11 @@
-export const myCache = (fn: any) => {
-  let cache = {};
-  return (...args: any) => {
+export const myCache = <R, T extends (...args: any[]) => R>(fn: T) => {
+  let cache: { [key: string]: R } = {};
+  return (...args: Parameters<T>): R => {
     const key = JSON.stringify(args);
-    // @ts-ignore
     if (!cache[key]) {
       console.log("not cached");
-      // @ts-ignore
       cache[key] = fn(...args);
     }
-    // @ts-ignore
     return cache[key];
   };
 };
